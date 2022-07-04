50bc900af9378c8ce1dfd3d4e26e586a89e2b0dc
Emilio Yoshimar Lopez Esponda <yoshi88mx@hotmail.com>
2022-07-04 10:32:38 -0500

    new games contacts

---
+++ RouletteGame.Core/GamesContracts/ILowHighGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteGame.Core.GamesContracts;
+public interface ILowHighGame
+{
+    Task<bool> IsMyLuckyDay(int bet);
+}
